c48be109dc8ac7e07db97b0cdc5870af419a7cc2
Husnain Saeed <husnainsaeed000@gmail.com>
2023-06-22 04:58:37 -0700

    Create MergArrays.py

---
+++ MergArrays.py
@@ -0,0 +1,30 @@
+class Solution:
+    def merge(self, nums1, m, nums2, n):
+        # Initialize pointers for nums1, nums2, and the merged array
+        p1, p2, p = m - 1, n - 1, m + n - 1
+
+        # Merge the arrays from the end
+        while p1 >= 0 and p2 >= 0:
+            if nums1[p1] <= nums2[p2]:
+                nums1[p] = nums2[p2]
+                p2 -= 1
+            else:
+                nums1[p] = nums1[p1]
+                p1 -= 1
+            p -= 1
+
+        # Copy the remaining elements from nums2 to nums1 if any
+        while p2 >= 0:
+            nums1[p] = nums2[p2]
+            p2 -= 1
+            p -= 1
+
+# Example usage:
+nums1 = [1, 2, 3, 0, 0, 0]
+m = 3
+nums2 = [2, 5, 6]
+n = 3
+
+solution = Solution()
+solution.merge(nums1, m, nums2, n)
+print(nums1)
